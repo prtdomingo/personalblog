71f5d4b89a958f6f4764dc9864be08b184b06cc1
Philip Domingo <v-phdomi@microsoft.com>
2019-04-10 00:53:48 +0800

    Added index.js

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM ghost:2-alpine
 RUN npm i -g applicationinsights
 RUN cd current && npm link applicationinsights
 
-COPY ./site-config.js /var/lib/ghost/current/
-
-RUN echo "require('./site-config.js');" >> /var/lib/ghost/current/index.js
+COPY ./index.js /var/lib/ghost/current/
---
+++ index.js
@@ -0,0 +1,62 @@
+// # Ghost Startup
+// Orchestrates the startup of Ghost when run from command line.
+
+var startTime = Date.now(),
+    debug = require('ghost-ignition').debug('boot:index'),
+    ghost, express, common, urlService, parentApp;
+
+debug('First requires...');
+
+ghost = require('./core');
+
+debug('Required ghost');
+
+express = require('express');
+common = require('./core/server/lib/common');
+urlService = require('./core/server/services/url');
+parentApp = express();
+const appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATION_KEY;
+
+if (appInsightsKey) {
+    const appInsights = require("applicationinsights");
+    appInsights.setup(appInsightsKey)
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true)
+        .setUseDiskRetryCaching(true)
+        .start();
+}
+
+debug('Initialising Ghost');
+ghost().then(function (ghostServer) {
+
+    // Mount our Ghost instance on our desired subdirectory path if it exists.
+    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
+
+    // All the URL config goes here
+    parentApp.all(/.*/, function(req, res, next) {
+        var host = req.header("host");
+
+        // add localhost as needed. No plans on running the app locally as of the moment
+        if (host.match(/^www\..*/i) || host.includes(".azurewebsites.net")) {
+            next();
+        } else {
+            res.redirect(301, "https://www." + host + req.url);
+        }
+    });
+
+    debug('Starting Ghost');
+    // Let Ghost handle starting our server instance.
+    return ghostServer.start(parentApp)
+        .then(function afterStart() {
+            common.logging.info('Ghost boot', (Date.now() - startTime) / 1000 + 's');
+        });
+}).catch(function (err) {
+    common.logging.error(err);
+    setTimeout(() => {
+        process.exit(-1);
+    }, 100);
+});
